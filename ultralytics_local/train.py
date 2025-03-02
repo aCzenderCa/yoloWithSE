365f04cb345a209478123c8c1cdd0760ad72688b
zender <ender_2997587939@outlook.com>
2025-03-02 16:05:49 +0800

    add multi_scale arg

--- ultralytics_local/train.py
+++ ultralytics_local/train.py
@@ -8,6 +8,7 @@ args.add_argument('--epoch', type=int, default=50)
 args.add_argument('--model', type=str, default='yolo11-obb-withTransform.yaml')
 args.add_argument('--scale', type=str, default='n')
 args.add_argument('--imgsz', type=int, default=640)
+args.add_argument('--multi_scale', type=bool, default=False)
 
 args = args.parse_args()
 
@@ -17,14 +18,14 @@ args.model = str.replace(args.model, 'yolo12', 'yolo12{0}', 1)
 if len(args.resume) == 0:
     model = YOLO(str.format(args.model, args.scale), task='obb')
 
-    results = model.train(data='DOTAv1.yaml', epochs=args.epoch, imgsz=args.imgsz)
+    results = model.train(data='DOTAv1.yaml', epochs=args.epoch, imgsz=args.imgsz, batch=0.8,
+                          multi_scale=args.multi_scale)
 else:
     model = YOLO(args.resume)
 
-    results = model.train(resume=True, epochs=args.epoch, imgsz=args.imgsz)
+    results = model.train(resume=True, epochs=args.epoch, imgsz=args.imgsz, batch=0.8, multi_scale=args.multi_scale)
 
 if model.trainer.best:
     model.best = model.trainer.best
 else:
     model.best = model.trainer.last
-
