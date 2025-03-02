e75fa3ee756991ddb267710d18db773d4cee1d69
zender <ender_2997587939@outlook.com>
2025-03-02 10:35:53 +0800

    fix model arg format

--- ultralytics_local/train.py
+++ ultralytics_local/train.py
@@ -5,10 +5,13 @@ args = ArgumentParser()
 
 args.add_argument('--resume', type=str, default='')
 args.add_argument('--epoch', type=int, default=2)
-args.add_argument('--model', type=str, default='yolo11{0}-obb-withTransform.yaml')
+args.add_argument('--model', type=str, default='yolo11-obb-withTransform.yaml')
 args.add_argument('--scale', type=str, default='n')
 
 args = args.parse_args()
+
+args.model = str.replace(args.model, 'yolo11', 'yolo11{0}', 1)
+args.model = str.replace(args.model, 'yolo12', 'yolo12{0}', 1)
 
 if len(args.resume) == 0:
     model = YOLO(str.format(args.model, args.scale), task='obb')
