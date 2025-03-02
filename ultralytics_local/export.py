d79de125492d04a540353de8f4b141426b8580e3
zender <ender_2997587939@outlook.com>
2025-03-02 11:39:04 +0800

    add export.py

---
+++ ultralytics_local/export.py
@@ -0,0 +1,12 @@
+from ultralytics_local.ultralytics import YOLO
+from argparse import ArgumentParser
+
+args = ArgumentParser()
+
+args.add_argument('--model', type=str, default='')
+
+args = args.parse_args()
+
+if len(args.model) > 0:
+    model = YOLO(args.model)
+    model.export(format='onnx')
