69ceac1d5b87751e52b4cfba478b4015af18c513
zender <ender_2997587939@outlook.com>
2025-03-05 13:09:43 +0800

    add no_val arg

--- ultralytics_local/train.py
+++ ultralytics_local/train.py
@@ -9,6 +9,7 @@ args.add_argument('--model', type=str, default='yolo11-obb-withTransform.yaml')
 args.add_argument('--scale', type=str, default='n')
 args.add_argument('--imgsz', type=int, default=640)
 args.add_argument('--multi_scale', type=bool, default=False)
+args.add_argument('--no_val', type=bool, action='store_true', default=False)
 
 args = args.parse_args()
 
@@ -19,11 +20,12 @@ if len(args.resume) == 0:
     model = YOLO(str.format(args.model, args.scale), task='obb')
 
     results = model.train(data='DOTAv1.yaml', epochs=args.epoch, imgsz=args.imgsz, batch=0.6,
-                          multi_scale=args.multi_scale)
+                          multi_scale=args.multi_scale, val=not args.no_val)
 else:
     model = YOLO(args.resume)
 
-    results = model.train(resume=True, epochs=args.epoch, imgsz=args.imgsz, batch=0.6, multi_scale=args.multi_scale)
+    results = model.train(resume=True, epochs=args.epoch, imgsz=args.imgsz, batch=0.6, multi_scale=args.multi_scale,
+                          val=not args.no_val)
 
 if model.trainer.best:
     model.best = model.trainer.best
