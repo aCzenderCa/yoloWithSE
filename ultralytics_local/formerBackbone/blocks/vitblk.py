d977da9cf870b2d76c5a72419f84633ab4db62e2
zender <ender_2997587939@outlook.com>
2025-03-11 22:03:19 +0800

    update

---
+++ ultralytics_local/formerBackbone/blocks/vitblk.py
@@ -0,0 +1,33 @@
+import torch
+from torch import nn
+
+from ultralytics.nn.modules import CBAM
+
+
+class ViTBlock(nn.Module):
+    def __init__(self, in_channel, out_channel, stride=None):
+        super().__init__()
+        ch_scale = out_channel / in_channel
+        if not stride:
+            stride = int(ch_scale) if ch_scale >= 1 else 1
+
+        self.dwconv = nn.Conv2d(in_channel, in_channel, kernel_size=3, stride=1, padding=1, groups=in_channel)
+        self.cbam = CBAM(in_channel)
+        self.bn0 = nn.BatchNorm2d(in_channel)
+        self.conv = nn.Conv2d(in_channel, int(in_channel * ch_scale), kernel_size=1, stride=stride)
+        self.act = nn.GELU()
+        self.ch_scale = ch_scale
+        self.stride = stride
+        self.bn1 = nn.BatchNorm2d(int(in_channel * ch_scale))
+
+    def forward(self, x: torch.Tensor):
+        raw_x = x
+        x = self.dwconv(x)
+        x = self.cbam(x)
+        x = x + raw_x
+        x = self.bn0(x)
+        x = self.act(x)
+
+        y = self.conv(x)
+        y = self.bn1(y)
+        return y
