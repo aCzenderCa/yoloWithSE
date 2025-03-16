765cef53b5ebe9cbd5c70191761cb14b338c7774
zender <ender_2997587939@outlook.com>
2025-03-16 19:32:22 +0800

    update

--- ultralytics_local/test.py
+++ ultralytics_local/test.py
@@ -1,5 +1,7 @@
 from argparse import ArgumentParser
+from ultralytics.utils.benchmarks import benchmark
 
+import torch
 from ultralytics import YOLO
 
 args = ArgumentParser()
@@ -7,9 +9,20 @@ args = ArgumentParser()
 args.add_argument('--model', type=str, default='best.pt')
 args.add_argument('--imgsz', type=int, default=1024)
 args.add_argument('--dataset', type=str, default='DOTAv1.5.yaml')
+args.add_argument('--only_model_structure', action='store_true', default=False)
 
 args = args.parse_args()
 
-model = YOLO(args.model, task='obb')
-metrics = model.val(data=args.dataset)
-print(f"mAP50:{metrics.box.map50}")
+args.model = str.split(args.model, '|')
+
+for model in args.model:
+    print(model, end="")
+    if args.only_model_structure:
+        model = YOLO(model, task='obb')
+        model.predict(torch.zeros(3, 3, args.imgsz, args.imgsz))
+    else:
+        benchmark(model=model, data=args.dataset, imgsz=args.imgsz)
+        metrics = model.val(data=args.dataset)
+        print(f"mAP50:{metrics.box.map50}")
+
+    print()
