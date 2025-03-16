67d612b4d313c8914fef972dcea4f9149b40b2e3
zender <ender_2997587939@outlook.com>
2025-03-16 19:53:22 +0800

    update

--- ultralytics_local/test.py
+++ ultralytics_local/test.py
@@ -10,6 +10,7 @@ args.add_argument('--model', type=str, default='best.pt')
 args.add_argument('--imgsz', type=int, default=1024)
 args.add_argument('--dataset', type=str, default='DOTAv1.5.yaml')
 args.add_argument('--only_model_structure', action='store_true', default=False)
+args.add_argument('--device', type=str, default="cuda:0")
 
 args = args.parse_args()
 
@@ -21,7 +22,7 @@ for model in args.model:
         model = YOLO(model, task='obb')
         model.predict(torch.zeros(3, 3, args.imgsz, args.imgsz))
     else:
-        benchmark(model=model, data=args.dataset, imgsz=args.imgsz, half=True)
+        benchmark(model=model, data=args.dataset, imgsz=args.imgsz, half=True, device=args.device)
         metrics = model.val(data=args.dataset)
         print(f"mAP50:{metrics.box.map50}")
 
