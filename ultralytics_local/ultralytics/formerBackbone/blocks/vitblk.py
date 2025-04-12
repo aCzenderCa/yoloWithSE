71db7b5f9be3e46ba9513632653a19a6f8a95605
zender <ender_2997587939@outlook.com>
2025-04-12 22:35:56 +0800

    update

--- ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
+++ ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
@@ -213,9 +213,6 @@ class ViTBlock6P(nn.Module):
         if stride > 1:
             self.scale.append(nn.MaxPool2d(5, stride, padding=2))
 
-        init.uniform_(self.dwconv.bias, -0.1, 0.1)
-        init.uniform_(self.cbam.channel_attention.fc.bias, -0.1, 0.1)
-
     def forward(self, x: torch.Tensor):
         raw_x = x
         x = self.small_blk(x)
