ab5411dce40fafd4493bcb9f14d2340274a71610
zender <ender_2997587939@outlook.com>
2025-04-12 20:59:20 +0800

    update

--- ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
+++ ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
@@ -259,7 +259,7 @@ class CA(nn.Module):
         a = self.pool(x)
         results = torch.zeros_like(x)
         for i in range(a.shape[0]):
-            results[i] = F.conv2d(x[i], a[i], padding=self.k // 2, groups=self.ch, stride=1)
+            results[i] = F.conv2d(x[i:i], a[i:i], padding=self.k // 2, groups=self.ch)
 
         return results
 
