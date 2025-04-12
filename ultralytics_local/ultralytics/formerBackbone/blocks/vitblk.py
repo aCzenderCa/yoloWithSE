d50f1229b90813131382ca2447f64ed7b653bebc
zender <ender_2997587939@outlook.com>
2025-04-12 19:40:24 +0800

    update

--- ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
+++ ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
@@ -242,10 +242,10 @@ class ViTBlockS1P(nn.Module):
         )
 
         gcd_for_out = math.gcd(self.extra_ch, in_channel)
-        print(gcd_for_out, self.extra_ch, in_channel)
         self.out_seq = nn.Sequential(
-            nn.Conv2d(in_channel, self.extra_ch, kernel_size=5, stride=stride, groups=gcd_for_out),
+            nn.Conv2d(in_channel, self.extra_ch, kernel_size=1, stride=stride),
             nn.BatchNorm2d(self.extra_ch),
+            nn.Conv2d(self.extra_ch, self.extra_ch, kernel_size=5, stride=stride, groups=self.extra_ch),
             CBAM(self.extra_ch),
         )
 
