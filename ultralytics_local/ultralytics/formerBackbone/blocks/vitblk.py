ac04543e939778dd49db8590794b1f3598152ee4
zender <ender_2997587939@outlook.com>
2025-04-12 20:30:26 +0800

    update

--- ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
+++ ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
@@ -251,11 +251,9 @@ class ViTBlockS1P(nn.Module):
     def __init__(self, in_channel, out_channel, stride=1, rep=1):
         super().__init__()
         self.token_mixer = nn.Sequential(
-            nn.Conv2d(in_channel, in_channel, kernel_size=1, stride=stride),
-            nn.BatchNorm2d(in_channel),
+            Insp(in_channel, [in_channel // 2, in_channel // 2], [(1, 3), (3, 1)], [(0, 1), (1, 0)], stride),
             CBAM(in_channel),
             nn.Conv2d(in_channel, out_channel, kernel_size=1),
-            nn.BatchNorm2d(out_channel),
         )
         self.channel_mixer = nn.Sequential(
             nn.Conv2d(out_channel, out_channel * 2, kernel_size=1, groups=out_channel),
