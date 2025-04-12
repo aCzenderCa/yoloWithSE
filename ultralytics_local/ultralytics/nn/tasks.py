68729db0210e3cdeabe5fcdf72e3e18f2aa6f867
zender <ender_2997587939@outlook.com>
2025-04-12 19:36:19 +0800

    update

--- ultralytics_local/ultralytics/nn/tasks.py
+++ ultralytics_local/ultralytics/nn/tasks.py
@@ -9,7 +9,8 @@ from pathlib import Path
 
 import torch
 
-from ultralytics.formerBackbone.blocks.vitblk import ViTBlock,ViTBlock2,ViTBlock3,ViTBlock4,ViTBlock5,ViTBlock6P
+from ultralytics.formerBackbone.blocks.vitblk import ViTBlock, ViTBlock2, ViTBlock3, ViTBlock4, ViTBlock5, ViTBlock6P, \
+    ViTBlockS1P
 
 from ultralytics.nn.modules import (
     AIFI,
@@ -995,6 +996,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             ViTBlock4,
             ViTBlock5,
             ViTBlock6P,
+            ViTBlockS1P,
         }
     )
     repeat_modules = frozenset(  # modules with 'repeat' arguments
