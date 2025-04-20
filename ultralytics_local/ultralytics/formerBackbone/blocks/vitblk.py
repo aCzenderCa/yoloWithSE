32ae8a0538493f529b699528db267adfb629b7e0
zender <ender_2997587939@outlook.com>
2025-04-20 17:06:06 +0800

    update

--- ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
+++ ultralytics_local/ultralytics/formerBackbone/blocks/vitblk.py
@@ -354,7 +354,7 @@ class MyTransLayer(nn.Module):
     def forward(self, x):
         _x = einops.reduce(x, "b c h w -> b 1 c", reduction="mean")
         _xs = torch.cat([self.linear0(_x), self.linear1(_x), self.linear2(_x), self.linear3(_x)], dim=1)
-        _xs = self.trans(_xs)
+        _xs = self.trans(_xs, _xs)
         _xs = einops.reduce(self.act(_xs), "b n c -> b c", reduction="mean")
 
         return _xs * x
