2443428721960669ce4e7f6b535be166f31c9062
zender <ender_2997587939@outlook.com>
2025-05-05 01:05:18 +0800

    update

--- ultralytics_local/ultralytics/utils/loss.py
+++ ultralytics_local/ultralytics/utils/loss.py
@@ -123,15 +123,8 @@ class RotatedBboxLoss(BboxLoss):
     def forward(self, pred_dist, pred_bboxes, anchor_points, target_bboxes, target_scores, target_scores_sum, fg_mask):
         """IoU loss."""
         weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
-        loss_iou = 0
-        for i in range(pred_bboxes.shape[0]):
-            print(fg_mask[i].shape)
-            print((pred_bboxes[i, :, 2] * pred_bboxes[i, :, 3]).shape)
-            iou = probiou(pred_bboxes[i, ...][fg_mask[i]], target_bboxes[i, ...][fg_mask[i]])
-            iou = 1 - iou
-            iou /= (pred_bboxes[i, :, 2] * pred_bboxes[i, :, 3] / pred_bboxes.shape[1] * 36)[fg_mask[i]]
-            loss_iou += (iou * weight).sum() / target_scores_sum
-        loss_iou /= pred_bboxes.shape[0]
+        iou = probiou(pred_bboxes[fg_mask], target_bboxes[fg_mask])
+        loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
 
         # DFL loss
         if self.dfl_loss:
