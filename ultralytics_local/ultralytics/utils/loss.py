21fcdd3f371157ff38351b693b3d9d64fcdfbb99
zender <ender_2997587939@outlook.com>
2025-05-04 22:40:00 +0800

    update

--- ultralytics_local/ultralytics/utils/loss.py
+++ ultralytics_local/ultralytics/utils/loss.py
@@ -100,6 +100,8 @@ class BboxLoss(nn.Module):
         """IoU loss."""
         weight = target_scores.sum(-1)[fg_mask].unsqueeze(-1)
         iou = bbox_iou(pred_bboxes[fg_mask], target_bboxes[fg_mask], xywh=False, CIoU=True)
+        print(iou)
+        print(iou.shape)
         loss_iou = ((1.0 - iou) * weight).sum() / target_scores_sum
 
         # DFL loss
