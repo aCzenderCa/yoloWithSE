85f972a26db9f528ea850abff8ad78f6be08b855
zender <ender_2997587939@outlook.com>
2025-05-05 14:37:11 +0800

    update

--- ultralytics_local/export.py
+++ ultralytics_local/export.py
@@ -4,9 +4,10 @@ from argparse import ArgumentParser
 args = ArgumentParser()
 
 args.add_argument('--model', type=str, default='')
+args.add_argument('--fmt', type=str, default='onnx')
 
 args = args.parse_args()
 
 if len(args.model) > 0:
     model = YOLO(args.model)
-    model.export(format='onnx')
+    model.export(format=args.fmt)
